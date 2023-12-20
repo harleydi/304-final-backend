b793b76106877856a8f92a004ef7c81ecb90573b
harleydi <disneyharley26@gmail.com>
2023-12-20 15:40:20 -0500

    added api to routes

--- app.js
+++ app.js
@@ -33,9 +33,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/case', casesRouter)
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/case', casesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
